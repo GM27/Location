b2d6fdaa01666caaeb7e9738d639f68b8aa5aacc
Jeremy M <63332982+GM27@users.noreply.github.com>
2023-11-26 14:45:57 -0800

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Jeremy Merezhko on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
