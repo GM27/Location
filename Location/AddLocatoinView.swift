040462770607476b95609b866952fb6cfdaa9422
Jeremy M <63332982+GM27@users.noreply.github.com>
2024-03-26 19:32:12 -0700

    new updates

--- Location/AddLocatoinView.swift
+++ Location/AddLocatoinView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import MapKit
 
-struct MainView: View {
+struct AddLocatoinView: View {
     var body: some View {
         
         VStack {
@@ -21,5 +21,5 @@ struct MainView: View {
 }
 
 #Preview {
-    MainView()
+    AddLocatoinView()
 }
